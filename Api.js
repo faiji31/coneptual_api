7436e799e521182b37e5881be0b8858fac732117
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-10-05 11:38:19 +0600

    3rd commit

--- Api.js
+++ Api.js
@@ -3,3 +3,16 @@ const arr =[1,2,3,4,5]
 arr.forEach(i=>{
     console.log(i,end=" ");
 });
+// for each amader kno kisu return kore nh(undefined return kore)
+
+// map
+const arrt = arr.map(d=>{
+   return d.arr
+})
+console.log(arrt)
+
+// Map notun array create korbe
+
+// Join
+const name =["faiji","akbar","liam"];
+console.log(name.join(" "))
